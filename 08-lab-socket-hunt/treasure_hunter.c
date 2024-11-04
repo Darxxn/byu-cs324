6c67daea6428745bf82c669ffcc36422efeb8169
Wai-Chun Stone Lai <wsl22@kasbah.cs.byu.edu>
2024-11-04 15:22:23 -0700

    Lab 3 Level 0 CP 4

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -24,6 +24,12 @@ keep a string verision of port as well (needed for getaddrinfo())
 store each argument provided on command line to main() in variables
 */
 int main(int argc, char *argv[]) {
+
+	if (argc < 5) {
+		fprintf(stderr, "Usage: %s server port level seed\n", argv[0]);
+		return 1;
+	}
+
 	char *server = argv[1];
 
 	char *port_str = argv[2];
@@ -50,6 +56,7 @@ int main(int argc, char *argv[]) {
 	memcpy(&message[6], &seed_network, sizeof(seed_network));
 
 	print_bytes(message, 8);
+	printf("\n");
 
     struct addrinfo hints, *res, *rp;
 	memset(&hints, 0, sizeof(hints));
@@ -77,22 +84,87 @@ int main(int argc, char *argv[]) {
 
 	// Send message using sendto
 	ssize_t bytes_sent = sendto(sockfd, message, sizeof(message), 0, remote_addr, sizeof(struct sockaddr_storage));
+
+	printf("\n");
+
 	if (bytes_sent == -1) {
-		perror("sendto");
+		perror("sendto\n");
 	} else {
 		printf("Send %zd bytes to %s:%s\n", bytes_sent, server, port_str);
 	}
+
+	printf("\n");
 
 	// Receive response using recvfrom
 	unsigned char response[256];
 	struct sockaddr_storage sender_addr;
 	socklen_t sender_addr_len = sizeof(sender_addr);
 	ssize_t bytes_received = recvfrom(sockfd, response, sizeof(response), 0, (struct sockaddr *)&sender_addr, &sender_addr_len);
+	printf("\n");
 	if (bytes_received == -1) {
 		perror("recvfrom");
 	} else {
 		printf("Received %zd bytes from server\n", bytes_received);
 		print_bytes(response, bytes_received);
+
+		unsigned char chunklen = response[0];
+
+		if (chunklen == 0) {
+			printf("Hunt is over. All chunks received\n");
+			close(sockfd);
+			return 0;
+		} else if (chunklen > 127) {
+			switch (chunklen) {
+				case 0x81: printf("Error: Message sent from an unexpected port.\n"); break;
+				case 0x82: printf("Error: Message sent to the wrong port.\n"); break;
+				case 0x83: printf("Error: Incorrect message length.\n"); break;
+				case 0x84: printf("Error: Incorrect nonce value.\n"); break;
+				case 0x85: printf("Error: Server failed to bind to address/port.\n"); break;
+				case 0x86: printf("Error: No remote port available for binding.\n"); break;
+				case 0x87: printf("Error: Bad level or initial byte in request.\n"); break;
+				case 0x88: printf("Error: Bad user ID in request.\n"); break;
+				case 0x89: printf("Error: Unknown error occurred.\n"); break;
+				case 0x8a: printf("Error: Wrong address family used.\n"); break;
+				default: printf("Error: Unrecognized error code.\n"); break;
+        	}
+			close(sockfd);
+			return 1;
+		}
+
+		// Extract the treasure chunk
+		char chunk[128];
+		memcpy(chunk, &response[1], chunklen);
+		// Null-terminate to print as a string
+		chunk[chunklen] = '\0';
+
+		// Extract the op-code, op-param, and nonce
+		unsigned char opcode = response[chunklen + 1];
+		unsigned char opparam = ntohs(*(unsigned short *)&response[chunklen + 2]);
+		unsigned int nonce = ntohl(*(unsigned int *)&response[chunklen + 4]);
+
+		// Print extracted values for verification
+		printf("\n");
+		printf("Chunk Length: %x\n", chunklen);
+		printf("Treasure Chunk: %s\n", chunk);
+		printf("Op Code: %x\n", opcode);
+		printf("Op Param: %x\n", opparam);  // Ignored for level 0
+		printf("Nonce: %x\n", nonce);
+
+		unsigned int next_nonce = nonce + 1;
+		unsigned char follow_up_message[4];
+		next_nonce = htonl(next_nonce);
+		memcpy(follow_up_message, &next_nonce, sizeof(next_nonce));
+
+		// printf("\n");
+		print_bytes(follow_up_message, 4);
+
+		ssize_t bytes_sent = sendto(sockfd, follow_up_message, sizeof(follow_up_message), 0, remote_addr, sizeof(struct sockaddr_storage));
+
+		if (bytes_sent == - 1) {
+			perror("sendto");
+			close(sockfd);
+			return 1;
+		}
 	}
 
 	// Clean up
