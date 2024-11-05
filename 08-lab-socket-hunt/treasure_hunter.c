87b9c46f267a19c20a38aa82a94498a5fdf3792e
Wai-Chun Stone Lai <wsl22@bulwark.cs.byu.edu>
2024-11-04 17:38:13 -0700

    Lab 3 Level 0 Checkpoint 2 done

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -24,12 +24,7 @@ keep a string verision of port as well (needed for getaddrinfo())
 store each argument provided on command line to main() in variables
 */
 int main(int argc, char *argv[]) {
-
-	if (argc < 5) {
-		fprintf(stderr, "Usage: %s server port level seed\n", argv[0]);
-		return 1;
-	}
-
+	// Checkpoint 0
 	char *server = argv[1];
 
 	char *port_str = argv[2];
@@ -38,16 +33,20 @@ int main(int argc, char *argv[]) {
 	int level = atoi(argv[3]);
 	int seed = atoi(argv[4]);
 
-	printf("Server: %s\n", server);
-	printf("Port (string): %s\n", port_str);
-	printf("Port (int): %d\n", port);
-	printf("Level: %d\n", level);
-	printf("Seed: %d\n", seed);
+	printf("server: %s\n", server);
+	printf("port (string): %s\n", port_str);
+	printf("port (int): %d\n", port);
+	printf("level: %d\n", level);
+	printf("seed: %d\n", seed);
 
+	// Checkpoint 1
 	unsigned char message[8];
 
+	// initialize buf to 0
+	bzero(message, 8);
+
 	message[0] = 0;
-	message[1] = (unsigned char)level;
+	message[1] = level;
 
 	unsigned int user_id = htonl(USERID);
 	memcpy(&message[2], &user_id, sizeof(user_id));
@@ -58,6 +57,7 @@ int main(int argc, char *argv[]) {
 	print_bytes(message, 8);
 	printf("\n");
 
+	// Checkpoint 2
     struct addrinfo hints, *res, *rp;
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
@@ -70,30 +70,30 @@ int main(int argc, char *argv[]) {
 	}
 
 	int sockfd;
-
 	struct sockaddr_storage remote_addr_ss;
 	struct sockaddr *remote_addr = (struct sockaddr *)&remote_addr_ss;
 
 	for (rp = res; rp != NULL; rp = rp->ai_next) {
 		sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
 		if (sockfd == -1) continue;
-
 		memcpy(remote_addr, rp->ai_addr, sizeof(struct sockaddr_storage));
+
+		char remote_ip[INET6_ADDRSTRLEN];
+		unsigned short remote_port;
+	    parse_sockaddr((struct sockaddr *)rp->ai_addr, remote_ip, &remote_port);
+		populate_sockaddr(remote_addr, rp->ai_family, remote_ip, remote_port);
 		break;
 	}
 
 	// Send message using sendto
 	ssize_t bytes_sent = sendto(sockfd, message, sizeof(message), 0, remote_addr, sizeof(struct sockaddr_storage));
 
-	printf("\n");
-
 	if (bytes_sent == -1) {
-		perror("sendto\n");
-	} else {
-		printf("Send %zd bytes to %s:%s\n", bytes_sent, server, port_str);
+		perror("sendto error\n");
 	}
-
-	printf("\n");
+	else {
+		printf("Sent %zd bytes to %s: %s\n", bytes_sent, server, port_str);
+	}
 
 	// Receive response using recvfrom
 	unsigned char response[256];
@@ -106,66 +106,10 @@ int main(int argc, char *argv[]) {
 	} else {
 		printf("Received %zd bytes from server\n", bytes_received);
 		print_bytes(response, bytes_received);
-
-		unsigned char chunklen = response[0];
-
-		if (chunklen == 0) {
-			printf("Hunt is over. All chunks received\n");
-			close(sockfd);
-			return 0;
-		} else if (chunklen > 127) {
-			switch (chunklen) {
-				case 0x81: printf("Error: Message sent from an unexpected port.\n"); break;
-				case 0x82: printf("Error: Message sent to the wrong port.\n"); break;
-				case 0x83: printf("Error: Incorrect message length.\n"); break;
-				case 0x84: printf("Error: Incorrect nonce value.\n"); break;
-				case 0x85: printf("Error: Server failed to bind to address/port.\n"); break;
-				case 0x86: printf("Error: No remote port available for binding.\n"); break;
-				case 0x87: printf("Error: Bad level or initial byte in request.\n"); break;
-				case 0x88: printf("Error: Bad user ID in request.\n"); break;
-				case 0x89: printf("Error: Unknown error occurred.\n"); break;
-				case 0x8a: printf("Error: Wrong address family used.\n"); break;
-				default: printf("Error: Unrecognized error code.\n"); break;
-        	}
-			close(sockfd);
-			return 1;
-		}
-
-		// Extract the treasure chunk
-		char chunk[128];
-		memcpy(chunk, &response[1], chunklen);
-		// Null-terminate to print as a string
-		chunk[chunklen] = '\0';
-
-		// Extract the op-code, op-param, and nonce
-		unsigned char opcode = response[chunklen + 1];
-		unsigned char opparam = ntohs(*(unsigned short *)&response[chunklen + 2]);
-		unsigned int nonce = ntohl(*(unsigned int *)&response[chunklen + 4]);
-
-		// Print extracted values for verification
-		printf("\n");
-		printf("Chunk Length: %x\n", chunklen);
-		printf("Treasure Chunk: %s\n", chunk);
-		printf("Op Code: %x\n", opcode);
-		printf("Op Param: %x\n", opparam);  // Ignored for level 0
-		printf("Nonce: %x\n", nonce);
-
-		unsigned int next_nonce = nonce + 1;
-		unsigned char follow_up_message[4];
-		next_nonce = htonl(next_nonce);
-		memcpy(follow_up_message, &next_nonce, sizeof(next_nonce));
-
-		// printf("\n");
-		print_bytes(follow_up_message, 4);
-
-		ssize_t bytes_sent = sendto(sockfd, follow_up_message, sizeof(follow_up_message), 0, remote_addr, sizeof(struct sockaddr_storage));
-
-		if (bytes_sent == - 1) {
-			perror("sendto");
-			close(sockfd);
-			return 1;
-		}
 	}
+
+	// Checkpoint 3
+	
 
 	// Clean up
 	freeaddrinfo(res);
